66ec1538ca647dd982314c6d86c9db5d80f97f4e
juliusrain <saining.li@gmail.com>
2012-10-21 16:56:57 -0300

    Changed main back (indentation problems wtf).

--- numbers.py
+++ numbers.py
@@ -17,28 +17,22 @@ def evaluate(code, guess):
     return "".join(feedback) 
             
 def main():
-   correct = False
-   code = []
-   numberOfTries = 0
-   for i in xrange(6):
+    correct = False
+    code = []
+    for i in xrange(6):
         code.append(str(random.randint(1,6)))
-   print """Guess the code. It contains the numbers 1-6 and is 6 numbers long. For each number you guess correctly, you'll get a "1" if it's in the correct position, and a "0" if it isn't."""
-   while not correct:
-		guess = list(raw_input(">> "))
-		if len(guess) != len(code):
-			   print "Your guess needs to be %s numbers long" % len(code)
-			   continue
-		feedback = evaluate(code, guess)
-		if feedback == "111111":
-				correct = True
-				print "Well, I guess you found out that the code is %s." % "".join(code)
-		else:
-				numberOfTries = numberOfTries + 1
-				if numberOfTries == 12:
-					print "Poison gass released, you are too late!"
-					break
-				print feedback
-   print "--------GAME OVER---------"
+    print """Guess the code. It contains the numbers 1-6 and is 6 numbers long. For each number you guess correctly, you'll get a "1" if it's in the correct position, and a "0" if it isn't."""
+    while not correct:
+        guess = list(raw_input(">> "))
+        if len(guess) != len(code):
+            print "Your guess needs to be %s numbers long" % len(code)
+            continue
+        feedback = evaluate(code, guess)
+        if feedback == "111111":
+            correct = True
+        else:
+            print feedback
+    print "Well, I guess you found out that the code is %s." % "".join(code)
 if __name__ == "__main__":
     main()
 
